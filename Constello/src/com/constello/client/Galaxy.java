d98be02671274b3572b1409228679a728442f203
Greg Tang <gerghk@stanford.edu>
2010-02-18 01:43:21 -0800

    Added Galaxy class

---
+++ Constello/src/com/constello/client/Galaxy.java
@@ -0,0 +1,12 @@
+package com.constello.client;
+
+import com.vaadin.contrib.gwtgraphics.client.DrawingArea;
+
+public class Galaxy extends DrawingArea {
+
+	public Galaxy(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
